c3c8214d9450beb3ed702b097779456960ea79ed
marcus gama <marcusgama@pop-os.localdomain>
2024-01-24 21:22:20 -0300

    :sparkles: Criando migration lead

---
+++ database/migrations/2024_01_24_233946_create_leads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('cpf',11)->unique();
+            $table->string('sindicato')->unique();
+            $table->string('status');          
+            $table->string('descricao');          
+            $table->string('telefone')->unique();          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
